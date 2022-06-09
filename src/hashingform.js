018ff06f31df5cf256d6e77cf300246b50db4aac
Jonas Bang <jonasbang@hotmail.dk>
2022-06-09 10:02:53 +0200

    Update hashingform.js

--- src/hashingform.js
+++ src/hashingform.js
@@ -8,12 +8,10 @@ export default function Hashing() {
     let [output, setOutput] = useState("")
 
     const handleTextInput = async (e) => {
-        // Get the value
         let value = e.target.value
 
         let result = ""
 
-        // Get the current active algorithm and hash the value using it.
         if (algorithm == "sha1") {
             result = await sha1(value)
         } else if (algorithm == "sha256") {
@@ -24,24 +22,19 @@ export default function Hashing() {
             result = await sha512(value)
         }
 
-        // Set the hashed text as output
         setOutput(result)
 
-        // Set the value of the text input
         setTextInput(value)
     }
 
     const handleAlgorithmChange = async (e) => {
 
-        // Get the selected algorithm
         let value = e.target.value;
     
         let result = '';
     
-        // Check if we have a text input
         if (text_input) {
     
-            // Hash the text based on the selected algorithm
             if (value == 'sha1') {
                 result = await sha1(text_input);
             } else if (value == 'sha256') {
@@ -56,26 +49,8 @@ export default function Hashing() {
     
         }
     
-        // Check if we have a file input
-        if (file_input) {
-    
-            // Hash the file content based on the selected algorithm
-            if (value == 'sha1') {
-                result = await sha1(file_input);
-            } else if (value == 'sha256') {
-                result = await sha256(file_input);
-            } else if (value == 'sha384') {
-                result = await sha384(file_input);
-            } else if (value == 'sha512') {
-                result = await sha512(file_input);
-            }
-    
-        }
-    
-        // Set the selected algorithm
         setAlgorithm(value);
     
-        // Set the hashed text
         setOutput(result);
     }
 
